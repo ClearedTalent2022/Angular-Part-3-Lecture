0149621af07932466be75dec5e788a168c1bd8c5
Pearce Odegard <69996490+podegard89@users.noreply.github.com>
2022-12-16 15:45:23 -0600

    Added code to the app component, Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title: string = "Angular-Part1";
+  // name: string = "Pearce Odegard";
+
+  author = {
+    name: "Pearce Odegard",
+    role: "Lead Instructor",
+    languages: [
+      "JavaScript",
+      "C#",
+      "Java"
+    ]
+  }
+}
