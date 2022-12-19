505c47ebf4ef085c5bb9e55c858c2e864a9179ef
Pearce Odegard <69996490+podegard89@users.noreply.github.com>
2022-12-19 15:13:08 -0600

    Added a remove skill button

--- src/app/skills-list/skills-list.component.ts
+++ src/app/skills-list/skills-list.component.ts
@@ -10,8 +10,23 @@ export class SkillsListComponent implements OnInit {
   languages: string[] = [
     "JavaScript",
     "C#",
-    "Java"
+    "Java",
+    "Python",
+    "TypeScript"
   ]
+
+  // Instead of having this function just
+  // log something to the console, let's
+  // have this function delete one of the 
+  // languages out of the languages array
+  removeSkill() {
+    // How do we remove an item from an array
+    // in javascript/typescript?
+    // There are a number of ways to remove
+    // elements from arrays, but let's just 
+    // remove the last item to keep it simple
+    this.languages.pop();
+  }
 
   constructor() { }
 
